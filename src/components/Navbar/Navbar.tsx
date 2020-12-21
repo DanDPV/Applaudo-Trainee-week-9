b8f67a082a49d9ad0a18bf988676ed72ab97d702
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 15:07:15 -0600

    :sparkles: Add base Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+/* eslint jsx-a11y/label-has-associated-control: 0 */
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
+import RouteNames from 'routers/RouteNames';
+
+const Navbar = () => (
+  <nav>
+    <button type="button" className="logo">
+      TM
+    </button>
+    <label htmlFor="toggle" className="menu-label">
+      <FontAwesomeIcon icon={faBars} />
+    </label>
+    <input name="toggle" id="toggle" type="checkbox" className="menu-button" />
+    <ul>
+      <li>
+        <NavLink className="link" exact to={RouteNames.Home}>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className="link" exact to={RouteNames.ViewCharacterPage}>
+          Characters
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Navbar;
