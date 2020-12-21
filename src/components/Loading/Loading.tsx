38636b66d6b5ff2cb51202ce273fe9f300677b44
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 14:47:59 -0600

    :sparkles: Add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'components/Loading/Loading.scss';
+
+const Loading = () => (
+  <div className="loading-container">
+    <h2>Loading...</h2>
+  </div>
+);
+
+export default Loading;
