78d41637f9420f9fc7d0cd09883017e31e7e9f11
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 15:26:08 -0600

    :sparkles: Use useWindowDimensions to change Navbar title dynamically

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,29 +5,39 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 import RouteNames from 'routers/RouteNames';
 import 'components/Navbar/Navbar.scss';
+import useWindowDimensions from 'hooks/useWindowDimensions';
 
-const Navbar = () => (
-  <nav>
-    <button type="button" className="logo logo-button">
-      Marvel Heroes
-    </button>
-    <label htmlFor="toggle" className="menu-label">
-      <FontAwesomeIcon icon={faBars} />
-    </label>
-    <input name="toggle" id="toggle" type="checkbox" className="menu-button" />
-    <ul>
-      <li>
-        <NavLink className="link" exact to={RouteNames.Home}>
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className="link" exact to={RouteNames.ViewCharacterPage}>
-          Characters
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <nav>
+      <button type="button" className="logo logo-button">
+        { width > 350 ? 'Marvel Heroes' : 'Marvel'}
+      </button>
+      <label htmlFor="toggle" className="menu-label">
+        <FontAwesomeIcon icon={faBars} />
+      </label>
+      <input
+        name="toggle"
+        id="toggle"
+        type="checkbox"
+        className="menu-button"
+      />
+      <ul>
+        <li>
+          <NavLink className="link" exact to={RouteNames.Home}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="link" exact to={RouteNames.ViewCharacterPage}>
+            Characters
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
