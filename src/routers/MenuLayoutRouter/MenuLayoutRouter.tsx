359ecf2ccc84b5cc85d2621cc8e40c743bf749c8
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 14:53:26 -0600

    :construction: Work on MenuLayoutRouter

--- src/routers/MenuLayoutRouter/MenuLayoutRouter.tsx
+++ src/routers/MenuLayoutRouter/MenuLayoutRouter.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import Loading from 'components/Loading/Loading';
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import RouteNames from 'routers/RouteNames';
 
-const MenuLayoutRouter = () => <h1>Menu Layout Router</h1>;
+const HomePage = lazy(() => import('pages/HomePage/HomePage'));
+const ListCharactersPage = lazy(() => import('pages/characters/ListCharactersPage'));
+
+const MenuLayoutRouter = () => (
+  <div className="page-container">
+    <div className="content-wrap">
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path={RouteNames.ListCharactersPage} component={ListCharactersPage} />
+          <Route path={RouteNames.Home} component={HomePage} />
+        </Switch>
+      </Suspense>
+    </div>
+  </div>
+);
 
 export default MenuLayoutRouter;
