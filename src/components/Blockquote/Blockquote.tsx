a7d84280d7713990f5d16f70aeea4ea4f84ee382
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 17:44:13 -0600

    :sparkles: Add Blockquote component

---
+++ src/components/Blockquote/Blockquote.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'components/Blockquote/Blockquote.scss';
+
+interface IBlockquote {
+  author: string;
+  quote: string;
+}
+
+const Blockquote = ({ author, quote }: IBlockquote) => (
+  <div className="quote">
+    <span className="left">❝</span>
+    <blockquote>{quote}</blockquote>
+    <small className="blockquote-author">{author}</small>
+    <span className="right">❞</span>
+  </div>
+);
+
+export default Blockquote;
