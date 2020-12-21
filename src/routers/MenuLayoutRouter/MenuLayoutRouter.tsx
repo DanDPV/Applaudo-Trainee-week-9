592502564e11c0b4a60a1036f0f51df42480b1b1
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 15:20:52 -0600

    :sparkles: Use Navbar in MenuLayoutRouter

--- src/routers/MenuLayoutRouter/MenuLayoutRouter.tsx
+++ src/routers/MenuLayoutRouter/MenuLayoutRouter.tsx
@@ -1,4 +1,5 @@
 import Loading from 'components/Loading/Loading';
+import Navbar from 'components/Navbar/Navbar';
 import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import RouteNames from 'routers/RouteNames';
@@ -9,6 +10,7 @@ const ListCharactersPage = lazy(() => import('pages/characters/ListCharactersPag
 const MenuLayoutRouter = () => (
   <div className="page-container">
     <div className="content-wrap">
+      <Navbar />
       <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path={RouteNames.ListCharactersPage} component={ListCharactersPage} />
