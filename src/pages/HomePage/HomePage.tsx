a6a6f833e785d124b6eae1ef803a069bcd38f897
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 16:45:30 -0600

    :construction: Add Fisrt row of HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import spiderman from 'static/spiderman.png';
+import 'pages/HomePage/HomePage.scss';
 
-const HomePage = () => <h1>Home Page</h1>;
+const HomePage = () => (
+  <div className="main-content">
+    <div className="intro-block">
+      <div className="intro-text">
+        The Marvel Universe is a fictional universe where the stories in most
+        American comic book titles and other media published by Marvel Comics
+        take place. Super-teams such as the Avengers, the X-Men, the Fantastic
+        Four, the Guardians of the Galaxy, the Defenders, the Midnight Sons, and
+        many Marvel superheroes live in this universe
+      </div>
+      <div className="intro-image">
+        <img src={spiderman} alt="spiderman" />
+      </div>
+    </div>
+  </div>
+);
 
 export default HomePage;
