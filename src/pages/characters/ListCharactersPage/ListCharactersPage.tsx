5a31ce64d6e5dfd5e60b7c57eaf19c39c837cfcc
Vicente De Paz <danconec7ion@gmail.com>
2020-12-22 13:35:57 -0600

    :sparkles: Display characters based on page url param

--- src/pages/characters/ListCharactersPage/ListCharactersPage.tsx
+++ src/pages/characters/ListCharactersPage/ListCharactersPage.tsx
@@ -1,3 +1,5 @@
+/* eslint no-shadow: 0 */
+/* eslint no-unused-vars: 0 */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { IRootState } from 'store/store';
@@ -6,11 +8,18 @@ import ICharacter from 'interfaces/ICharacter';
 import useFetch from 'hooks/useFetch';
 import Card from 'components/Card/Card';
 import Loading from 'components/Loading/Loading';
+import { useHistory } from 'react-router-dom';
+import { getQueryVariable } from 'utils/utils';
 import 'pages/characters/ListCharactersPage/ListCharactersPage.scss';
 
 const ListCharactersPage = () => {
+  enum QuerysParams {
+    Page = 'page',
+  }
+
   const dispatch = useDispatch();
-  const { url } = useSelector((state: IRootState) => state.search);
+  const history = useHistory();
+  const { url, limit } = useSelector((state: IRootState) => state.search);
   const { loading, data: genericResponse, error } = useFetch<IGenericApiResponse<ICharacter>>(url);
 
   const { data } = genericResponse ?? {};
@@ -30,6 +39,21 @@ const ListCharactersPage = () => {
       });
     };
   }, []);
+
+  useEffect(() => {
+    const newPage = getQueryVariable(QuerysParams.Page)
+      ? +getQueryVariable(QuerysParams.Page)
+      : 1;
+
+    const newOffset = Math.ceil((newPage - 1) * limit);
+
+    dispatch({
+      type: 'SET_OFFSET',
+      payload: {
+        offset: newOffset,
+      },
+    });
+  }, [history.location, limit]);
 
   return (
     <div className="main-content mb-5">
