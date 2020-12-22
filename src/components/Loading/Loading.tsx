8c92946103c3583ecf5083da76f6a7728e44b626
Vicente De Paz <danconec7ion@gmail.com>
2020-12-22 11:52:20 -0600

    :lipstick: Add dr strange image to loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import drStrangeSprite from 'static/dr-strange-sprite.gif';
 import 'components/Loading/Loading.scss';
 
 const Loading = () => (
   <div className="loading-container">
+    <img src={drStrangeSprite} alt="dr stange" className="loading-image" />
     <h2>Loading...</h2>
+    <img src={drStrangeSprite} alt="dr stange" className="loading-image flip-image" />
   </div>
 );
 
