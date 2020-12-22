83901be1728dd564267555f09b42e230039a548d
Vicente De Paz <danconec7ion@gmail.com>
2020-12-22 14:01:42 -0600

    :sparkles: Add Pagination component

--- src/pages/characters/ListCharactersPage/ListCharactersPage.tsx
+++ src/pages/characters/ListCharactersPage/ListCharactersPage.tsx
@@ -2,14 +2,16 @@
 /* eslint no-unused-vars: 0 */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import queryString from 'query-string';
+import { useHistory } from 'react-router-dom';
 import { IRootState } from 'store/store';
 import IGenericApiResponse from 'interfaces/IGenericApiResponse';
 import ICharacter from 'interfaces/ICharacter';
 import useFetch from 'hooks/useFetch';
 import Card from 'components/Card/Card';
 import Loading from 'components/Loading/Loading';
-import { useHistory } from 'react-router-dom';
 import { getQueryVariable } from 'utils/utils';
+import Pagination from 'components/Pagination/Pagination';
 import 'pages/characters/ListCharactersPage/ListCharactersPage.scss';
 
 const ListCharactersPage = () => {
@@ -19,11 +21,17 @@ const ListCharactersPage = () => {
 
   const dispatch = useDispatch();
   const history = useHistory();
-  const { url, limit } = useSelector((state: IRootState) => state.search);
+  const { url, limit, offset } = useSelector((state: IRootState) => state.search);
   const { loading, data: genericResponse, error } = useFetch<IGenericApiResponse<ICharacter>>(url);
 
   const { data } = genericResponse ?? {};
-  const { results } = data ?? {};
+  const { results, total } = data ?? {};
+
+  const handleChangePage = (newPage: number) => history.push(
+    `?${queryString.stringify({
+      page: newPage,
+    })}`,
+  );
 
   useEffect(() => {
     dispatch({
@@ -78,6 +86,14 @@ const ListCharactersPage = () => {
           )}
         </div>
       </div>
+      {!loading && !error && results && results.length > 0 && (
+        <Pagination
+          offset={offset}
+          limit={limit}
+          total={total ?? 0}
+          onChange={handleChangePage}
+        />
+      )}
     </div>
   );
 };
