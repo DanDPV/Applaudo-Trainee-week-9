fd1edc5dbc63cf1b8da0f5ecb2be14e1621c6cea
Vicente De Paz <danconec7ion@gmail.com>
2020-12-22 12:07:27 -0600

    :lipstick: Add bop animation to loading text

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -5,7 +5,7 @@ import 'components/Loading/Loading.scss';
 const Loading = () => (
   <div className="loading-container">
     <img src={drStrangeSprite} alt="dr stange" className="loading-image" />
-    <h2 className="loading-text">Loading...</h2>
+    <h2 className="loading-text bop">Loading...</h2>
     <img src={drStrangeSprite} alt="dr stange" className="loading-image flip-image" />
   </div>
 );
