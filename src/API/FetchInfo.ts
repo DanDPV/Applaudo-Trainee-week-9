513062008260bc70c51660e8ac44158ead97b8f6
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 19:17:02 -0600

    :sparkles: Add FetchInfo API helper

---
+++ src/API/FetchInfo.ts
@@ -0,0 +1,30 @@
+/* eslint no-undef: 0 */
+async function http<T>(path: string, config: RequestInit): Promise<T> {
+  const request = new Request(path, config);
+  const response = await fetch(request);
+  if (!response.ok) {
+    throw new Error('Could not load info');
+  }
+  // may error if there is no body, return empty array
+  return response.json().catch(() => ({}));
+}
+export async function get<T>(path: string, config?: RequestInit): Promise<T> {
+  const init = { method: 'get', ...config };
+  return http<T>(path, init);
+}
+export async function post<T, U>(
+  path: string,
+  body: T,
+  config?: RequestInit,
+): Promise<U> {
+  const init = { method: 'post', body: JSON.stringify(body), ...config };
+  return http<U>(path, init);
+}
+export async function put<T, U>(
+  path: string,
+  body: T,
+  config?: RequestInit,
+): Promise<U> {
+  const init = { method: 'put', body: JSON.stringify(body), ...config };
+  return http<U>(path, init);
+}
