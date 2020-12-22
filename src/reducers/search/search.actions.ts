3b343b5556ec739197db8f51bc726a92fa587482
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 22:08:35 -0600

    :sparkles: Add SearchActions

---
+++ src/reducers/search/search.actions.ts
@@ -0,0 +1,38 @@
+interface setOffset {
+  readonly type: 'SET_OFFSET';
+  payload: {
+    offset: number;
+  };
+}
+
+interface setLimit {
+  readonly type: 'SET_LIMIT';
+  payload: {
+    limit: number;
+  };
+}
+
+interface setTotal {
+  readonly type: 'SET_TOTAL';
+  payload: {
+    total: number;
+  };
+}
+
+interface setCount {
+  readonly type: 'SET_COUNT';
+  payload: {
+    count: number;
+  };
+}
+
+interface setBaseUrl {
+  readonly type: 'SET_BASE_URL';
+  payload: {
+    baseUrl: string;
+  };
+}
+
+type SearchActions = setOffset | setLimit | setTotal | setCount | setBaseUrl;
+
+export default SearchActions;
