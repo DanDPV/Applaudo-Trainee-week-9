20b86fcc5fbe46ef45858bc2883c35f4c5314809
Vicente De Paz <danconec7ion@gmail.com>
2020-12-22 17:34:57 -0600

    :recycle: Add and export ISetName and ISetAllParams action creators

--- src/reducers/search/search.actions.ts
+++ src/reducers/search/search.actions.ts
@@ -19,10 +19,25 @@ export interface ISetBaseUrl {
   };
 }
 
+export interface ISetName {
+  readonly type: 'SET_NAME';
+  payload: {
+    name: string;
+  };
+}
+
+export interface ISetAllParams {
+  readonly type: 'SET_ALL_PARAMS';
+  payload: {
+    offset: number;
+    name: string;
+  };
+}
+
 export interface IReset {
   readonly type: 'RESET';
 }
 
-type SearchActions = ISetOffset | ISetLimit | ISetBaseUrl | IReset;
+type SearchActions = ISetOffset | ISetLimit | ISetBaseUrl | ISetName | ISetAllParams | IReset;
 
 export default SearchActions;
