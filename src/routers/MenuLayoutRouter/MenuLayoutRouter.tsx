ce9c0a099c1f0b60d326972011fc9666e4eb5f9b
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 18:01:16 -0600

    :lipstick: Import MenuLayoutRouter style

--- src/routers/MenuLayoutRouter/MenuLayoutRouter.tsx
+++ src/routers/MenuLayoutRouter/MenuLayoutRouter.tsx
@@ -3,6 +3,7 @@ import Navbar from 'components/Navbar/Navbar';
 import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import RouteNames from 'routers/RouteNames';
+import 'routers/MenuLayoutRouter/MenuLayoutRouter.scss';
 
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
 const ListCharactersPage = lazy(() => import('pages/characters/ListCharactersPage'));
