66343aca083c52ee3b00e92cabbd3baea87d8044
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 18:09:25 -0600

    :sparkles: Add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import RouteNames from 'routers/RouteNames';
+import 'components/Footer/Footer.scss';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer-right">
+      <a href="https://www.facebook.com/Marvel/">
+        <FontAwesomeIcon icon={faFacebookF} />
+      </a>
+      <a href="https://twitter.com/marvel">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+    </div>
+
+    <div className="footer-left">
+      <p className="footer-links">
+        <NavLink exact to={RouteNames.Home} className="link-1 footer-link">
+          Home
+        </NavLink>
+        <NavLink
+          exact
+          to={RouteNames.ListCharactersPage}
+          className="footer-link"
+        >
+          Characters
+        </NavLink>
+      </p>
+      <p>Marvel &copy; 2020</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
