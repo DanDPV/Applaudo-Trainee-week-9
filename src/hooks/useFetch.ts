6a987efedf59dc1bdf03710683fc1b537ba657e8
Vicente De Paz <danconec7ion@gmail.com>
2020-12-21 22:35:10 -0600

    :sparkles: Add useFetch hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,45 @@
+/* eslint arrow-body-style:  */
+import { useEffect, useRef, useState } from 'react';
+import { get } from 'API/FetchInfo';
+
+const useFetch = <T>(url: string) => {
+  const isMounted = useRef(true);
+  type resp = {
+    data: T | null;
+    loading: boolean;
+    error: string | null;
+  };
+  const [state, setState] = useState(<resp>{
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({ data: null, loading: true, error: null });
+
+    get<T>(url)
+      .then(data => {
+        if (isMounted.current) {
+          setState(previusState => ({ ...previusState, loading: false, data }));
+        }
+      })
+      .catch(() => {
+        setState({
+          data: null,
+          loading: false,
+          error: 'Can not load info',
+        });
+      });
+  }, [url]);
+
+  return state;
+};
+
+export default useFetch;
