4c76b25cfc2767a0041ac9ef3423c5c544498ada
Vicente De Paz <danconec7ion@gmail.com>
2020-12-22 22:38:08 -0600

    :sparkles: Add getFirstNStories helper

--- src/helpers/fetchService.ts
+++ src/helpers/fetchService.ts
@@ -1,8 +1,8 @@
-/* eslint-disable import/prefer-default-export */
 import queryString from 'query-string';
 import { get } from 'API/FetchInfo';
 import IGenericApiResponse from 'interfaces/IGenericApiResponse';
 import IComic from 'interfaces/IComic';
+import IStory from 'interfaces/IStory';
 
 const getFirstNComics = async (limit: number) => {
   try {
@@ -20,4 +20,20 @@ const getFirstNComics = async (limit: number) => {
   }
 };
 
-export { getFirstNComics };
+const getFirstNStories = async (limit: number) => {
+  try {
+    const data = await get<IGenericApiResponse<IStory>>(
+      `${process.env.REACT_APP_API_URL}v1/public/stories?${queryString.stringify(
+        {
+          limit,
+          apikey: process.env.REACT_APP_PUBLIC_KEY,
+        },
+      )}`,
+    );
+    return data;
+  } catch (error) {
+    return null;
+  }
+};
+
+export { getFirstNComics, getFirstNStories };
