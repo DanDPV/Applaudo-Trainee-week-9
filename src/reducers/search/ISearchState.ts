7f3360ddbd584eb8b27f5cb46b8c1746eb034a5a
Vicente De Paz <danconec7ion@gmail.com>
2020-12-23 18:22:57 -0600

    :recycle: Add properties to save the result state

--- src/reducers/search/ISearchState.ts
+++ src/reducers/search/ISearchState.ts
@@ -1,3 +1,6 @@
+import ICharacter from 'interfaces/ICharacter';
+import IGenericApiResponse from 'interfaces/IGenericApiResponse';
+
 interface ISearchState {
   offset: number;
   limit: number;
@@ -6,6 +9,9 @@ interface ISearchState {
   name: string;
   comic: string;
   story: string;
+  loading: boolean;
+  error: string;
+  data: IGenericApiResponse<ICharacter> | null;
 }
 
 export default ISearchState;
