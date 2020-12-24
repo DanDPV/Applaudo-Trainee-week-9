102b4f04aba046fbcaecc03452edb481d81d5a0f
Vicente De Paz <danconec7ion@gmail.com>
2020-12-23 19:12:03 -0600

    :recycle: Add loading, error and data properties to ISearchComicState

--- src/reducers/searchComic/ISearchComicState.ts
+++ src/reducers/searchComic/ISearchComicState.ts
@@ -1,3 +1,6 @@
+import IComic from 'interfaces/IComic';
+import IGenericApiResponse from 'interfaces/IGenericApiResponse';
+
 interface ISearchComicState {
   offset: number;
   limit: number;
@@ -5,6 +8,9 @@ interface ISearchComicState {
   url: string;
   format: string;
   title: string;
+  loading: boolean;
+  error: string;
+  data: IGenericApiResponse<IComic> | null;
 }
 
 export default ISearchComicState;
