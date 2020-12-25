a1f49918ae8e8209f0c9640a87debc4cb56f70e6
Vicente De Paz <danconec7ion@gmail.com>
2020-12-25 13:00:42 -0600

    :recycle: Add ListStoriesPage route name

--- src/routers/RouteNames.ts
+++ src/routers/RouteNames.ts
@@ -1,10 +1,11 @@
 /* eslint no-shadow: 0 */
 /* eslint no-unused-vars: 0 */
 enum RouteNames {
-    Home = '/',
-    ListCharactersPage = '/characters',
-    ViewCharacterPage = '/characters/:idCharacter',
-    ListComicsPage = '/comics',
+  Home = '/',
+  ListCharactersPage = '/characters',
+  ViewCharacterPage = '/characters/:idCharacter',
+  ListComicsPage = '/comics',
+  ListStoriesPage = '/stories',
 }
 
 export default RouteNames;
