dafc64175d4800c1d6f878bb7f79a3c1a66b187c
Vicente De Paz <danconec7ion@gmail.com>
2020-12-26 17:36:49 -0600

    :recycle: Add custom route to main router

--- src/routers/AppRouter/AppRouter.tsx
+++ src/routers/AppRouter/AppRouter.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-import ViewCharacterPage from 'pages/characters/ViewCharacterPage';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Loading from 'components/Loading/Loading';
 import MenuLayoutRouter from 'routers/MenuLayoutRouter/MenuLayoutRouter';
+import FooterLayoutRoute from 'routers/FooterLayoutRoute/FooterLayoutRoute';
 import RouteNames from 'routers/RouteNames';
+
+const ViewCharacterPage = lazy(() => import('pages/characters/ViewCharacterPage/ViewCharacterPage'));
 
 const AppRouter = () => (
   <Router>
-    <Switch>
-      <Route exact path={RouteNames.ViewCharacterPage} component={ViewCharacterPage} />
-      <Route path={RouteNames.Home} component={MenuLayoutRouter} />
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <FooterLayoutRoute
+          exact
+          path={RouteNames.ViewCharacterPage}
+          component={ViewCharacterPage}
+        />
+        <Route path={RouteNames.Home} component={MenuLayoutRouter} />
+      </Switch>
+    </Suspense>
   </Router>
 );
 
