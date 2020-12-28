6c6c3a2d4c79d19e7f51daa6f7bba823fbfd9ced
Vicente De Paz <danconec7ion@gmail.com>
2020-12-28 13:33:12 -0600

    :sparkles: Redirect to item main page while clicking

--- src/components/CustomOrderedList/CustomOrderedList.tsx
+++ src/components/CustomOrderedList/CustomOrderedList.tsx
@@ -1,19 +1,27 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { shortenText } from 'utils/utils';
 import IExtendedObjectItem from 'interfaces/common/IExtendedObjectItem';
 import 'components/CustomOrderedList/CustomOrderedList.scss';
 
 interface ICustomOrderedList {
-    items: IExtendedObjectItem[];
+  items: IExtendedObjectItem[];
+  onClick(id: string): void;
 }
 
-const CustomOrderedList = ({ items }: ICustomOrderedList) => (
+const CustomOrderedList = ({ items, onClick }: ICustomOrderedList) => (
   <ol className="custom-ol">
-    {items.map(item => (
-      <li className="custom-li" key={item.resourceURI}>
-        {shortenText(item.name, 30)}
-      </li>
-    ))}
+    {items.map(item => {
+      const pathVars = item.resourceURI.split('/');
+      const resourceId = pathVars[pathVars.length - 1];
+      return (
+        <li className="custom-li" key={item.resourceURI} onClick={() => onClick(resourceId)}>
+          {shortenText(item.name, 30)}
+        </li>
+      );
+    })}
   </ol>
 );
 
