905b69b24c14ea8830a79bccfcef5273ea6f99f0
Vicente De Paz <danconec7ion@gmail.com>
2020-12-29 13:57:17 -0600

    :recycle: Wrap AppRouter with PersistGate component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import AppRouter from 'routers/AppRouter/AppRouter';
-import store from 'store/store';
+import store, { persistor } from 'store/store';
+import Loading from 'components/Loading/Loading';
 
 const App = () => (
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate loading={<Loading />} persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>
 );
 
