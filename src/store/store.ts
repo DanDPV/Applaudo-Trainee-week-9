9aba95d73f900db0e4264d3aac32e488ebacef7c
Vicente De Paz <danconec7ion@gmail.com>
2020-12-29 13:56:41 -0600

    :recycle: Configure redux-persist in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,18 @@
 /* eslint-disable no-underscore-dangle */
 import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import searchReducer from 'reducers/search/searchReducer';
 import searchComicReducer from 'reducers/searchComic/searchComicReducer';
 import searchStoryReducer from 'reducers/searchStory/searchStoryReducer';
 import viewItemReducer from 'reducers/viewItem/viewItemReducer';
 import localItemsReducer from 'reducers/localItems/localItemsReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['localItems'],
+};
 
 const rootReducer = combineReducers({
   search: searchReducer,
@@ -14,11 +22,15 @@ const rootReducer = combineReducers({
   localItems: localItemsReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-export type IRootState = ReturnType<typeof rootReducer>
+export const persistor = persistStore(store);
+
+export type IRootState = ReturnType<typeof rootReducer>;
 
 export default store;
