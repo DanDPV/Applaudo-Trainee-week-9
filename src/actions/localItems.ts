3ba234f2559a6e2d4c02957e9e8f12f32bc353ee
Vicente De Paz <danconec7ion@gmail.com>
2020-12-29 14:36:35 -0600

    :sparkles: Add addBookmark action creator

--- src/actions/localItems.ts
+++ src/actions/localItems.ts
@@ -1,8 +1,15 @@
 import { ILocalItem } from 'reducers/localItems/ILocalItemsState';
-import { IHideLocalItem, ILocalItemsReset } from 'reducers/localItems/localItems.actions';
+import { IAddBookmark, IHideLocalItem, ILocalItemsReset } from 'reducers/localItems/localItems.actions';
 
 const hideLocalItem = (item: ILocalItem): IHideLocalItem => ({
   type: 'HIDE_LOCAL_ITEM',
+  payload: {
+    item,
+  },
+});
+
+const addBookmark = (item: ILocalItem): IAddBookmark => ({
+  type: 'ADD_BOOKMARK',
   payload: {
     item,
   },
@@ -14,5 +21,6 @@ const resetLocalItems = (): ILocalItemsReset => ({
 
 export {
   hideLocalItem,
+  addBookmark,
   resetLocalItems,
 };
