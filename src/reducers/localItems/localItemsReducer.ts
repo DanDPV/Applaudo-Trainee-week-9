aad2399b479755c74b2b2c0619eb43b5afd9c18e
Vicente De Paz <danconec7ion@gmail.com>
2020-12-29 11:55:23 -0600

    :sparkles: Add localItemsReducer

---
+++ src/reducers/localItems/localItemsReducer.ts
@@ -0,0 +1,32 @@
+/* eslint no-case-declarations:0 */
+
+import ILocalItemsState from 'reducers/localItems/ILocalItemsState';
+import LocalItemsActions from 'reducers/localItems/localItems.actions';
+
+const initState = {
+  hiddenItems: [],
+  bookmarks: [],
+};
+
+const localItemsReducer = (
+  state: ILocalItemsState = initState,
+  action: LocalItemsActions,
+) => {
+  switch (action.type) {
+    case 'HIDE_LOCAL_ITEM':
+      return {
+        ...state,
+        hiddenItems: [...state.hiddenItems, action.payload.item],
+      };
+
+    case 'RESET_LOCAL_ITEMS':
+      return {
+        ...initState,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default localItemsReducer;
