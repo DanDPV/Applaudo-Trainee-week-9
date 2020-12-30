a80ca9e24ec877cc36b081e7281be9eca74d7125
Vicente De Paz <danconec7ion@gmail.com>
2020-12-29 20:51:09 -0600

    :sparkles: Implement ciew, hide, add and remove actions

--- src/pages/bookmarks/StoryBookmarks/StoryBookmarks.tsx
+++ src/pages/bookmarks/StoryBookmarks/StoryBookmarks.tsx
@@ -1,20 +1,40 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
+import { faBookmark as faBookmarkSolid } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as faBookmarkRegular } from '@fortawesome/free-regular-svg-icons';
+import { useHistory } from 'react-router-dom';
 import queryString from 'query-string';
 import IStory from 'interfaces/IStory';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { IRootState } from 'store/store';
 import { get } from 'API/FetchInfo';
 import IGenericApiResponse from 'interfaces/IGenericApiResponse';
 import { imagePlaceholder } from 'utils/globals';
 import Card from 'components/Card/Card';
+import { addBookmark, hideLocalItem, removeBookmark } from 'actions/localItems';
 import 'pages/bookmarks/common/styles.scss';
 
 const StoryBookmarks = () => {
   const { hiddenItems, bookmarks } = useSelector((state: IRootState) => state.localItems);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   const [stories, setStories] = useState<IStory[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const handleViewMore = (id: number) => history.push(`/comics/${id}`);
+
+  const handleHideItem = (id: number) => {
+    dispatch(hideLocalItem({ id, type: 'STORY' }));
+    setStories(char => [...char.filter(c => c.id !== id)]);
+  };
+
+  const handleAddBookmark = (id: number) => dispatch(addBookmark({ id, type: 'STORY' }));
+
+  const handleRemoveBookmark = (id: number) => {
+    dispatch(removeBookmark({ id, type: 'STORY' }));
+    setStories(char => [...char.filter(c => c.id !== id)]);
+  };
 
   useEffect(() => {
     setLoading(true);
