9ba980bad82f8e968cb2d9caf47d0693f4bb6f9a
Vicente De Paz <danconec7ion@gmail.com>
2020-12-29 18:08:15 -0600

    :sparkles: Show characters info

--- src/pages/bookmarks/CharacterBookmarks/CharacterBookmarks.tsx
+++ src/pages/bookmarks/CharacterBookmarks/CharacterBookmarks.tsx
@@ -1,18 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { faBookmark as faBookmarkSolid } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as faBookmarkRegular } from '@fortawesome/free-regular-svg-icons';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import queryString from 'query-string';
 import { IRootState } from 'store/store';
 import { get } from 'API/FetchInfo';
 import IGenericApiResponse from 'interfaces/IGenericApiResponse';
 import ICharacter from 'interfaces/ICharacter';
+import { imagePlaceholder } from 'utils/globals';
+import Card from 'components/Card/Card';
+import { addBookmark, hideLocalItem, removeBookmark } from 'actions/localItems';
 import 'pages/bookmarks/common/styles.scss';
 
 const CharacterBookmarks = () => {
   const { bookmarks } = useSelector((state: IRootState) => state.localItems);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
   const [characters, setCharacters] = useState<ICharacter[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const handleViewMore = (id: number) => history.push(`/characters/${id}`);
+
+  const handleHideItem = (id: number) => dispatch(hideLocalItem({ id, type: 'CHARACTER' }));
+
+  const handleAddBookmark = (id: number) => dispatch(addBookmark({ id, type: 'CHARACTER' }));
+
+  const handleRemoveBookmark = (id: number) => dispatch(removeBookmark({ id, type: 'CHARACTER' }));
 
   useEffect(() => {
     setLoading(true);
@@ -35,6 +51,30 @@ const CharacterBookmarks = () => {
     <div className="main-content mb-5">
       <div className="bookmarks-title-div">
         <h1>Character Bookmarks</h1>
+      </div>
+      <div className="cards">
+        <div className="cards-content">
+          {!loading
+            && characters
+            && characters.map(char => {
+              const inBookmark = bookmarks.find(item => item.type === 'CHARACTER' && item.id === char.id);
+              return (
+                <Card
+                  key={char.id}
+                  id={char.id}
+                  name={char.name}
+                  description={char.description ?? ''}
+                  bookmarkIcon={inBookmark ? faBookmarkSolid : faBookmarkRegular}
+                  handleViewMore={handleViewMore}
+                  handleHideItem={handleHideItem}
+                  handleBookmarkAction={inBookmark ? handleRemoveBookmark : handleAddBookmark}
+                  imageUrl={char.thumbnail
+                    ? `${char.thumbnail.path}/portrait_uncanny.${char.thumbnail.extension}`
+                    : imagePlaceholder}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
