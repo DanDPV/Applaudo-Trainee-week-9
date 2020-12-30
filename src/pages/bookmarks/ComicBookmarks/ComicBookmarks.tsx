89bf4dac8e8f669d0267bd6f442ecc44348d41c5
Vicente De Paz <danconec7ion@gmail.com>
2020-12-29 20:14:34 -0600

    :construction: Show comics on cards

--- src/pages/bookmarks/ComicBookmarks/ComicBookmarks.tsx
+++ src/pages/bookmarks/ComicBookmarks/ComicBookmarks.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
+import { faBookmark as faBookmarkSolid } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as faBookmarkRegular } from '@fortawesome/free-regular-svg-icons';
 import { useSelector } from 'react-redux';
 import queryString from 'query-string';
 import IComic from 'interfaces/IComic';
 import { IRootState } from 'store/store';
 import { get } from 'API/FetchInfo';
 import IGenericApiResponse from 'interfaces/IGenericApiResponse';
+import { imagePlaceholder } from 'utils/globals';
+import Card from 'components/Card/Card';
+import 'pages/bookmarks/common/styles.scss';
 
 const ComicBookmarks = () => {
   const { hiddenItems, bookmarks } = useSelector((state: IRootState) => state.localItems);
@@ -40,6 +45,30 @@ const ComicBookmarks = () => {
     <div className="main-content mb-5">
       <div className="bookmarks-title-div">
         <h1>Character Bookmarks</h1>
+      </div>
+      <div className="cards">
+        <div className="cards-content">
+          {!loading
+            && comics
+            && comics.map(comic => {
+              const inBookmark = bookmarks.find(item => item.type === 'COMIC' && item.id === comic.id);
+              return (
+                <Card
+                  key={comic.id}
+                  id={comic.id}
+                  name={comic.title}
+                  description={comic.description ?? ''}
+                  bookmarkIcon={inBookmark ? faBookmarkSolid : faBookmarkRegular}
+                  handleViewMore={handleViewMore}
+                  handleHideItem={handleHideItem}
+                  handleBookmarkAction={inBookmark ? handleRemoveBookmark : handleAddBookmark}
+                  imageUrl={comic.thumbnail
+                    ? `${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`
+                    : imagePlaceholder}
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
