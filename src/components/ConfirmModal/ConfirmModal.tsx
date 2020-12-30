8eaf2887815d24246ac0fa3e6afb4d8b9258ec79
Vicente De Paz <danconec7ion@gmail.com>
2020-12-30 12:56:02 -0600

    :sparkles: Add ConfirmModal component

---
+++ src/components/ConfirmModal/ConfirmModal.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+import React, { ReactNode } from 'react';
+import ReactDOM from 'react-dom';
+import 'components/ConfirmModal/ConfirmModal.scss';
+
+interface IConfirmModal {
+  title: string;
+  children: ReactNode;
+  confirmText: string;
+  mainColor: string;
+  open: boolean;
+  handleAction(confirmed: boolean): void;
+}
+
+const ConfirmModal = ({
+  title,
+  children,
+  confirmText,
+  mainColor,
+  open,
+  handleAction,
+}: IConfirmModal) => {
+  if (!open) return null;
+
+  return ReactDOM.createPortal(
+    <>
+      <div className="modal" />
+      <div className="modal-content">
+        <div className="modal-header">
+          <button
+            type="button"
+            className="close"
+            onClick={() => handleAction(false)}
+          >
+            &times;
+          </button>
+          <h2>{title}</h2>
+        </div>
+        <div className="modal-body">{children}</div>
+        <div className="modal-footer">
+          <button
+            type="button"
+            className="modal-btn confirm"
+            onClick={() => handleAction(true)}
+          >
+            {confirmText}
+          </button>
+          <button
+            type="button"
+            className="modal-btn"
+            onClick={() => handleAction(false)}
+          >
+            cancel
+          </button>
+        </div>
+      </div>
+    </>,
+    document.getElementById('modal-portal')!,
+  );
+};
+
+export default ConfirmModal;
