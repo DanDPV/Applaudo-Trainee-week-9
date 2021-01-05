40755633d11499c5fea0702d0894532933dbedfb
Vicente De Paz <danconec7ion@gmail.com>
2021-01-05 16:09:31 -0600

    :white_check_mark: Add mock store set name test

--- src/pages/characters/ListCharactersPage/ListCharactersPage.test.tsx
+++ src/pages/characters/ListCharactersPage/ListCharactersPage.test.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 /* eslint-disable no-undef */
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import configureStore from 'redux-mock-store';
 import store from 'store/store';
+import { setAllParams, setName } from 'actions/search';
 import ListCharactersPage from './ListCharactersPage';
 
 describe('Test on ListCharactersPage component', () => {
@@ -26,9 +29,9 @@ describe('Test on ListCharactersPage component', () => {
     });
   });
 
-  test('should filtered characters by name', async () => {
-    const searchValue = 'spid';
-    const { container } = render(
+  test('should show typed name on input', async () => {
+    const search = 'spid';
+    render(
       <Provider store={store}>
         <BrowserRouter>
           <ListCharactersPage />
@@ -36,12 +39,61 @@ describe('Test on ListCharactersPage component', () => {
       </Provider>,
     );
 
+    userEvent.type(screen.getByPlaceholderText(/Hero´s name/i), search);
+    expect((screen.getByPlaceholderText(/Hero´s name/i) as HTMLInputElement).value).toBe(search);
+  });
+});
+
+describe('Mock store tests', () => {
+  const mockStore = configureStore();
+
+  const initialState = {
+    search: {
+      offset: 0,
+      limit: 12,
+      baseUrl: '',
+      url: '',
+      name: '',
+      comic: '',
+      story: '',
+      loading: false,
+      error: '',
+      data: null,
+    },
+    localItems: {
+      hiddenItems: [],
+      bookmarks: [],
+    },
+  };
+  const testStore = mockStore(initialState);
+
+  testStore.dispatch = jest.fn();
+
+  jest.mock('actions/search', () => ({
+    setBaseUrl: jest.fn(),
+    setName: jest.fn(),
+    setAllParams: jest.fn(),
+  }));
+
+  beforeEach(() => {
+    testStore.clearActions();
+    jest.clearAllMocks();
+  });
+
+  test('should call set name dispatch while typing name', async () => {
+    const searchValue = 'spid';
+    render(
+      <Provider store={testStore}>
+        <MemoryRouter>
+          <ListCharactersPage />
+        </MemoryRouter>
+      </Provider>,
+    );
+
     userEvent.type(screen.getByPlaceholderText(/Hero´s name/i), searchValue);
 
-    await waitFor(() => {
-      expect(container.querySelector('.card')).not.toBeNull();
-      expect(container.querySelector('.loading-container')).toBeNull();
-      expect((screen.getByPlaceholderText(/Hero´s name/i) as HTMLInputElement).value).toBe(searchValue);
-    });
+    const searchValueArray = searchValue.split('');
+    // eslint-disable-next-line max-len
+    searchValueArray.forEach(letter => expect(testStore.dispatch).toHaveBeenCalledWith(setName(letter)));
   });
 });
