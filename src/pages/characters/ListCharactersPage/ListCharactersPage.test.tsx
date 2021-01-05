cab658f04875377ddbd61adc112c59301052439d
Vicente De Paz <danconec7ion@gmail.com>
2021-01-05 13:59:39 -0600

    :recycle: Wait for info to be fetched to match snapshot

--- src/pages/characters/ListCharactersPage/ListCharactersPage.test.tsx
+++ src/pages/characters/ListCharactersPage/ListCharactersPage.test.tsx
@@ -1,21 +1,28 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
 /* eslint-disable no-undef */
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import store from 'store/store';
 import ListCharactersPage from './ListCharactersPage';
 
 describe('Test on ListCharactersPage component', () => {
-  test('Should render component correctly', () => {
-    const container = render(
+  test('Should render component correctly', async () => {
+    const { container } = render(
       <Provider store={store}>
         <BrowserRouter>
           <ListCharactersPage />
         </BrowserRouter>
       </Provider>,
     );
-    expect(container.baseElement).toMatchSnapshot();
+
+    await waitFor(() => {
+      expect(container.querySelector('.card')).not.toBeNull();
+      expect(container.querySelector('.loading-container')).toBeNull();
+      expect(container).toMatchSnapshot();
+    });
   });
 });
