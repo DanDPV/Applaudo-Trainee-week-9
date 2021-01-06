f31f6e20a12effb52b8748da82592be31bf3393f
Vicente De Paz <danconec7ion@gmail.com>
2021-01-06 11:33:54 -0600

    :white_check_mark: Add ViewCharacterPage base tests

---
+++ src/pages/characters/ViewCharacterPage/ViewCharacterPage.test.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-undef */
+import React from 'react';
+import { Provider } from 'react-redux';
+import { waitFor } from '@testing-library/react';
+import store from 'store/store';
+import { renderWithRouter } from 'tests/utils';
+import ViewCharacterPage from './ViewCharacterPage';
+
+describe('Test in ViewCharacterPage component', () => {
+  test('should render component correctly', async () => {
+    const { container } = renderWithRouter(
+      <Provider store={store}>
+        <ViewCharacterPage />
+      </Provider>, { route: '/characters/1' },
+    );
+
+    await waitFor(() => {
+      expect(container.querySelector('.image-header-title')).not.toBeNull();
+      expect(container.querySelector('.image-header-title')?.innerHTML).toBe('spiderman');
+      expect(container).toMatchSnapshot();
+    });
+  });
+});
