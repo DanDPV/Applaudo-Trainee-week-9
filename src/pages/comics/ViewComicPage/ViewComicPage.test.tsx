faddb32ce9af828b1ec8e979d40dfe43c4f41e82
Vicente De Paz <danconec7ion@gmail.com>
2021-01-06 13:42:29 -0600

    :white_check_mark: Add hide item test

--- src/pages/comics/ViewComicPage/ViewComicPage.test.tsx
+++ src/pages/comics/ViewComicPage/ViewComicPage.test.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/extensions */
 /* eslint-disable no-undef */
 import React from 'react';
 import { Provider } from 'react-redux';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
 import store from 'store/store';
 import { renderWithRouter } from 'tests/utils';
+import { renderWithCustomHistory } from 'tests/componentUtils';
 import ViewComicPage from './ViewComicPage';
 
 describe('Test on ViewComicPage component', () => {
@@ -56,5 +59,25 @@ describe('Test on ViewComicPage component', () => {
     expect(screen.getByText(/Remove bookmark/i)).toBeInTheDocument();
     userEvent.click(screen.getByText(/Remove bookmark/i));
     expect(screen.getByText(/Add bookmark/i)).toBeInTheDocument();
+  });
+
+  test('should hide item', async () => {
+    const history = createMemoryHistory();
+    history.push('/comics');
+    history.push('/comics/1');
+    const { container } = renderWithCustomHistory(history);
+
+    await waitFor(() => {
+      expect(container.querySelector('.image-header-title')).not.toBeNull();
+    });
+
+    expect(screen.getByText(/Hide item/i)).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Hide item/i));
+    await waitFor(() => {
+      expect(screen.getByText(/Search your comic/i)).toBeInTheDocument();
+      expect(container.querySelector('.card')).toBeNull();
+      expect(container.querySelector('.loading-container')).toBeNull();
+      expect(screen.getByText(/Comics on this page are hidden 🤐/i)).toBeInTheDocument();
+    });
   });
 });
