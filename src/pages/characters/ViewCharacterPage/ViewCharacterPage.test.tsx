887f553940a7a57a651db4306e4fa5f9d6cbd5a2
Vicente De Paz <danconec7ion@gmail.com>
2021-01-06 11:52:51 -0600

    :white_check_mark: Add show comic and story list test

--- src/pages/characters/ViewCharacterPage/ViewCharacterPage.test.tsx
+++ src/pages/characters/ViewCharacterPage/ViewCharacterPage.test.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { Provider } from 'react-redux';
-import { waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import store from 'store/store';
 import { renderWithRouter } from 'tests/utils';
 import ViewCharacterPage from './ViewCharacterPage';
@@ -20,5 +20,21 @@ describe('Test in ViewCharacterPage component', () => {
       expect(container.querySelector('.image-header-title')?.innerHTML).toBe('spiderman');
       expect(container).toMatchSnapshot();
     });
+  });
+
+  test('should show comic and story list correctly', async () => {
+    const { container } = renderWithRouter(
+      <Provider store={store}>
+        <ViewCharacterPage />
+      </Provider>,
+      { route: '/characters/1' },
+    );
+
+    await waitFor(() => {
+      expect(container.querySelector('.image-header-title')).not.toBeNull();
+    });
+
+    expect(screen.getByText(/Spiderman Comic/i)).toBeInTheDocument();
+    expect(screen.getByText(/Spiderman Story/i)).toBeInTheDocument();
   });
 });
