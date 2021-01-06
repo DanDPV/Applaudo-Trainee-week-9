5143d1d8983e48dfa4b56465d75f0dede925e720
Vicente De Paz <danconec7ion@gmail.com>
2021-01-06 11:22:40 -0600

    :recycle: Use mock api responses on mock api handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,78 +1,37 @@
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable arrow-body-style */
 import { rest } from 'msw';
+import {
+  characterResponse,
+  charactersResponse,
+  comicsResponse,
+  storiesResponse,
+} from './testData';
 
 const handlers = [
   rest.get(`${process.env.REACT_APP_API_URL}v1/public/characters`, (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({
-        data: {
-          offset: 0,
-          limit: 12,
-          total: 1,
-          count: 1,
-          results: [
-            {
-              id: 1,
-              name: 'spiderman',
-              description: 'description',
-              thumbnail: {
-                path: 'path',
-                extension: 'jpg',
-              },
-            },
-          ],
-        },
-      }),
+      ctx.json(charactersResponse()),
+    );
+  }),
+  rest.get(`${process.env.REACT_APP_API_URL}v1/public/characters/:idCharacter`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json(characterResponse()),
     );
   }),
   rest.get(`${process.env.REACT_APP_API_URL}v1/public/comics`, (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({
-        data: {
-          offset: 0,
-          limit: 12,
-          total: 1,
-          count: 1,
-          results: [
-            {
-              id: 1,
-              title: 'comic spiderman',
-              description: 'description',
-              thumbnail: {
-                path: 'path',
-                extension: 'jpg',
-              },
-            },
-          ],
-        },
-      }),
+      ctx.json(comicsResponse()),
     );
   }),
   rest.get(`${process.env.REACT_APP_API_URL}v1/public/stories`, (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({
-        data: {
-          offset: 0,
-          limit: 12,
-          total: 1,
-          count: 1,
-          results: [
-            {
-              id: 1,
-              title: 'story spiderman',
-              description: 'description',
-              thumbnail: {
-                path: 'path',
-                extension: 'jpg',
-              },
-            },
-          ],
-        },
-      }),
+      ctx.json(storiesResponse()),
     );
   }),
 ];
