3c388eab2b5ab53cf6bf78eaa0ab6c85280ed625
Vicente De Paz <danconec7ion@gmail.com>
2021-01-06 16:51:36 -0600

    :white_check_mark: Add Add/Remove bookmark test

--- src/pages/stories/ViewStoryPage/ViewStoryPage.test.tsx
+++ src/pages/stories/ViewStoryPage/ViewStoryPage.test.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import store from 'store/store';
 import { renderWithRouter } from 'tests/utils';
 import ViewStoryPage from './ViewStoryPage';
@@ -38,5 +39,24 @@ describe('Test on ViewStoryPage', () => {
 
     expect(screen.getByText(/Char Spiderman/i)).toBeInTheDocument();
     expect(screen.getByText(/Spiderman Comic/i)).toBeInTheDocument();
+  });
+
+  test('should Add/Remove bookmark', async () => {
+    const { container } = renderWithRouter(
+      <Provider store={store}>
+        <ViewStoryPage />
+      </Provider>,
+      { route: '/stories/1' },
+    );
+
+    await waitFor(() => {
+      expect(container.querySelector('.image-header-title')).not.toBeNull();
+    });
+
+    expect(screen.getByText(/Add bookmark/i)).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Add bookmark/i));
+    expect(screen.getByText(/Remove bookmark/i)).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Remove bookmark/i));
+    expect(screen.getByText(/Add bookmark/i)).toBeInTheDocument();
   });
 });
