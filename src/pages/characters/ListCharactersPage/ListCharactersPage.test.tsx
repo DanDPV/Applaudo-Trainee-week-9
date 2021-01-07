9d46fe8e5176a8b0ed3081d52a455e6175c9df02
Vicente De Paz <danconec7ion@gmail.com>
2021-01-06 19:42:06 -0600

    :white_check_mark: Add handle api error test

--- src/pages/characters/ListCharactersPage/ListCharactersPage.test.tsx
+++ src/pages/characters/ListCharactersPage/ListCharactersPage.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/extensions */
@@ -7,10 +8,13 @@ import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent, { TargetElement } from '@testing-library/user-event';
+import { rest } from 'msw';
 import configureStore from 'redux-mock-store';
+
 import store from 'store/store';
 import { setName } from 'actions/search';
 import { addBookmark } from 'actions/localItems';
+import server from 'mocks/server';
 import ListCharactersPage from './ListCharactersPage';
 
 describe('Test on ListCharactersPage component', () => {
@@ -84,6 +88,30 @@ describe('Test on ListCharactersPage component', () => {
     userEvent.click(container.querySelector('.btn-hide') as TargetElement);
 
     expect(container.querySelector('.card')).toBeNull();
+  });
+
+  test('should handle api error', async () => {
+    server.use(
+      rest.get(`${process.env.REACT_APP_API_URL}v1/public/characters`, (req, res, ctx) => {
+        return res(
+          ctx.status(404),
+        );
+      }),
+    );
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ListCharactersPage />
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(/Could not load characters 😓/i)).toBeInTheDocument();
+      expect(container.querySelector('.card')).toBeNull();
+      expect(container.querySelector('.pagination-options')).toBeNull();
+    });
   });
 });
 
